0d94762216697bfef66d2902a8b35efa09f4b387
Giantblargg <ddoran3@gmail.com>
2014-02-17 18:21:58 -0700

    added launcher

---
+++ src/siliconClaymore/subsystems/LauncherRaw.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package siliconClaymore.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ *
+ * @author daniel
+ */
+public class LauncherRaw {
+    
+    SpeedController motor;
+
+    public LauncherRaw(SpeedController motor) {
+	this.motor = motor;
+    }
+    
+    public void FIRE () {
+	
+    }
+    
+}
