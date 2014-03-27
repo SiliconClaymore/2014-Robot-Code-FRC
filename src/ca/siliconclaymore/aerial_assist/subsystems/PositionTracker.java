08300b0858e6ba91e76e18d6d1481c45ba8114a1
GiantBlargg <ddoran3@gmail.com>
2014-03-26 20:46:31 -0600

    add javadoc to PositionTracker as well as defining an alternate constructor

--- src/ca/siliconclaymore/aerial_assist/subsystems/PositionTracker.java
+++ src/ca/siliconclaymore/aerial_assist/subsystems/PositionTracker.java
@@ -8,26 +8,55 @@ package ca.siliconclaymore.aerial_assist.subsystems;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
+ * Class to handle the position of rotating arms using only Limit Switches.
  *
  * @author Daniel
  */
 public class PositionTracker {
 
-    private DigitalInput[] positions;
+    private final DigitalInput[] positions;
     private int lastPosition;
 
-    public PositionTracker(DigitalInput[] positions, int lastPosition) {
+    /**
+     * Creates an instance of PositionTracker.
+     *
+     * @param positions An array of {@link DigitalInput} representing Limit
+     * Switches in order.
+     * @param startPosition The last known position or supposed starting
+     * position. Even numbers signify contact with a sensor (array position * 2)
+     * while odd numbers signify no contact with a sensor. If unknown just set
+     * this to whatever.
+     */
+    public PositionTracker(DigitalInput[] positions, int startPosition) {
         this.positions = positions;
-        this.lastPosition = lastPosition;
+        lastPosition = startPosition;
     }
 
     /**
-     * Returns the predicted position Make sure to call this often even if not
-     * reading the result Calling to often is not a problem
-     * @param forward Specify what direction moving in
-     * @return Position as an int
-     * even numbers signify contact with a sensor (array position * 2)
-     * while odd numbers signify no contact with a sensor
+     * Creates an instance of PositionTracker.
+     *
+     * @param ports An array of ints representing ports that are connected to
+     * Limit Switches in order.
+     * @param startPosition The last known position or supposed starting
+     * position. Even numbers signify contact with a sensor (array position * 2)
+     * while odd numbers signify no contact with a sensor. If unknown just set
+     * this to whatever.
+     */
+    public PositionTracker(int[] ports, int startPosition) {
+        positions = new DigitalInput[ports.length];
+        for (int i = 0; i < ports.length; i++) {
+            positions[i] = new DigitalInput(ports[i]);
+        }
+        this.lastPosition = startPosition;
+    }
+
+    /**
+     * Returns the predicted position. Make sure to call this often even if not
+     * reading the result. Calling too often is not a problem
+     *
+     * @param forward Specify what direction moving in.
+     * @return Position as an int. Even numbers signify contact with a sensor
+     * (array position * 2) while odd numbers signify no contact with a sensor.
      */
     public int update(boolean forward) {
         int workingPos = -1;
