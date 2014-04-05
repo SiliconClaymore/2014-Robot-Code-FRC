62f9d4ef10c6bb2518cb9de4876fff3af0d6cfdb
Giantblargg <ddoran3@gmail.com>
2014-04-04 21:34:35 -0600

    fixed new autonomous

--- src/ca/siliconclaymore/aerial_assist/subsystems/auto/AutoCTRL.java
+++ src/ca/siliconclaymore/aerial_assist/subsystems/auto/AutoCTRL.java
@@ -7,31 +7,48 @@ package ca.siliconclaymore.aerial_assist.subsystems.auto;
 
 import ca.siliconclaymore.aerial_assist.subsystems.SubsystemController;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
  *
  * @author daniel
  */
-public class AutoCTRL implements SubsystemController{
-    
+public class AutoCTRL implements SubsystemController {
+
     RobotDrive drive;
+    SpeedController launcher;
     double driveTime;
     double driveSpeed;
+    double shootTime;
     Timer timer;
+    boolean driving;
 
-    public AutoCTRL(RobotDrive drive, double driveTime, double driveSpeed) {
+    public AutoCTRL(RobotDrive drive, SpeedController launcher, double driveTime, double driveSpeed, double shootTime) {
 	this.drive = drive;
+	this.launcher = launcher;
 	this.driveTime = driveTime;
 	this.driveSpeed = driveSpeed;
+	this.shootTime = shootTime;
 	(this.timer = new Timer()).start();
+	driving = true;
     }
-    
-    public void update () {
-	if (driveTime>timer.get()) {
-	    drive.tankDrive(driveSpeed, driveSpeed);
+
+    public void update() {
+	if (driving) {
+	    if (driveTime > timer.get()) {
+		drive.tankDrive(driveSpeed, driveSpeed);
+	    } else {
+		drive.stopMotor();
+		driving = false;
+	    }
 	} else {
-	    drive.stopMotor();
+	    if (shootTime > timer.get()) {
+		launcher.set(1);
+	    } else {
+		launcher.set(0);
+	    }
 	}
+
     }
 }
